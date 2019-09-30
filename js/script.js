a755c4ca1a21858bf241f4d231725b3048d5c436
mohiduzzaman <m.mohid1997@gmail.com>
2019-10-01 00:05:12 +0600

    files updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-$(function(){
-// About part slider
-$('.about-slider').slick({
+$(function ($) {
+    
+    //about part slider
+  $('.about-slider').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
@@ -9,19 +10,36 @@ $('.about-slider').slick({
     
  responsive: [
     {
-      breakpoint: 576,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
       }
     },
     {
-      breakpoint: 481,
+      breakpoint: 576,
       settings: {
         slidesToShow: 1,
       }
     } 
   ]    
 });
+
+    //down to top button
+    $(".bto").click(function () {
+        $("html,body").animate({
+            scrollTop: 0,
+        }, 1000);
+    });
+
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 80) {
+            $(".bto").fadeIn();
+        } else {
+            $(".bto").fadeOut();
+        }
+    });
     
-    
+
 });
